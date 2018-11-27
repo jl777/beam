70e77c9e4f6bb008a708dd7cc84b308eeced6d87
Vadim Grigoruk <vadim@beam-mw.com>
2018-11-27 18:04:07 +0300

    wallet api: test balance() apiwallet api: can open wallet.dbwallet api: added commad line parameterswallet api: renamed files

--- wallet/unittests/api_client.cpp
+++ wallet/unittests/api_client.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "wallet_api_client.h"
-
 #include "utility/helpers.h"
 
 #include "utility/io/tcpserver.h"
