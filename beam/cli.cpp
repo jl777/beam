8f223c39e084e09b7928061c7b11d0f11ba740dd
jl777 <jameslee777@yahoo.com>
2018-12-24 01:29:44 -1100

    libcli

--- beam/cli.cpp
+++ beam/cli.cpp
@@ -317,7 +317,7 @@ int main_impl(int argc, char* argv[])
     return 0;
 }
 
-int main(int argc, char* argv[]) {
+int beam_main(int argc, char* argv[]) {
 #ifdef _WIN32
     return main_impl(argc, argv);
 #else
