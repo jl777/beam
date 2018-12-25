15bbd9ed9eb434cb5bca995f06c02753c06bfd85
jl777 <jameslee777@yahoo.com>
2018-12-24 23:41:22 -1100

    Recvaddr

--- wallet/cli.cpp
+++ wallet/cli.cpp
@@ -638,7 +638,8 @@ int main_impl(int argc, char* argv[])
                             }
 
 							receiverWalletID.FromHex(vm[cli::RECEIVER_ADDR].as<string>());
-
+                            auto recvaddress = m_WalletDB->getAddress(receiverWalletID);
+                            fprintf(stderr,"recvaddress.(%s)\n",std::to_string(address.m_walletID));
                             auto signedAmount = vm[cli::AMOUNT].as<double>();
                             if (signedAmount < 0)
                             {
