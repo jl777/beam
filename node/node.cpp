2326785425b1ff2e7f272995b7929ae8e0744c3d
jl777 <jameslee777@yahoo.com>
2018-12-24 18:43:19 -1100

    Reverse polarity

--- node/node.cpp
+++ node/node.cpp
@@ -738,7 +738,7 @@ void Node::Initialize(IExternalPOW* externalPOW)
     LOG_INFO() << "Initial Tip: " << m_Processor.m_Cursor.m_ID;
     fprintf(stderr,"check treasury %d %d\n",m_Cfg.m_Treasury.empty(),m_Processor.m_Extra.m_TreasuryHandled);
 
-    if (!m_Cfg.m_Treasury.empty() && !m_Processor.m_Extra.m_TreasuryHandled)
+    if (m_Cfg.m_Treasury.empty() && !m_Processor.m_Extra.m_TreasuryHandled)
     {
         fprintf(stderr,"init treasury\n");
         m_Processor.OnTreasury(Blob(m_Cfg.m_Treasury));
